ee6979909ff63b6521aa850db39381461ab7beec
Aravindb02 <aravindachar826@gmail.com>
2024-10-15 14:46:36 +0530

    third

--- script.js
+++ script.js
@@ -1,23 +1,25 @@
 function nameFunction(event) {
-    event.preventDefault(); // Prevent the form from submitting
+    event.preventDefault();
 
     const firstName = document.getElementById('firstname').value;
     const lastName = document.getElementById('lastname').value;
     const firstNameInput = document.getElementById('firstname');
     const lastNameInput = document.getElementById('lastname');
+    document.getElementById('textarea').innerHTML = firstName+" "+lastName;
 
     if (firstName.length < 6) {
         firstNameInput.style.borderColor = 'red';
-        alert('First name must be atleast 6 characters long!');
+        alert('First name must be atleast 6 characters long');
     }
-    else if (lastName.length < 6 || lastName.length > 12) {
-        lastNameInput.style.borderColor = 'red';
-        alert('First name must be between 6 and 10 characters long!');
+    if (lastName.length < 3 || lastName.length > 12) {
+        lastNameInput.style.borderColor = 'yellow';
+        alert('Last name must be between 6 and 12 characters long');
     }
     else if (firstName === '' || lastName === '') {
-        alert('Both first name and last name are required!');
+        alert('Both first name and last name are required');
     }
     else {
-        alert('Form submitted successfully!');
+        alert('successful');
     }
+
 }
